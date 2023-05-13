b04e087fcfabce97e644199c84c28e6b7f72999d
Nick Kohrn <bnkohrn@icloud.com>
2023-05-13 09:37:29 -0400

    Add package files

---
+++ README.md
@@ -0,0 +1,3 @@
+# UserDefaultsKit
+
+A description of this package.
---
+++ Sources/UserDefaultsKit/UserDefaultsKit.swift
@@ -0,0 +1,6 @@
+public struct UserDefaultsKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
