f3e2e7b896cbf98a69d45065a680a2107275b95f
Nick Kohrn <bnkohrn@icloud.com>
2023-05-31 09:15:59 -0400

    Add logging

--- Sources/UserDefaultsKit/UserDefaultsClient+DependencyKey.swift
+++ Sources/UserDefaultsKit/UserDefaultsClient+DependencyKey.swift
@@ -1,25 +1,84 @@
 import ComposableArchitecture
 import Foundation
+import OSLog
 import XCTestDynamicOverlay
 
 extension UserDefaultsClient: DependencyKey {
+    private static let logger = Logger(
+        subsystem: Bundle.main.bundleIdentifier!,
+        category: String(describing: Self.self)
+    )
+
     public static let liveValue: Self = {
         return Self(
-            arrayForKey: { defaults, key in defaults.array(forKey: key) },
-            boolForKey: { defaults, key in defaults.bool(forKey: key) },
-            dataForKey: { defaults, key in defaults.data(forKey: key) },
-            doubleForKey: { defaults, key in defaults.double(forKey: key) },
-            integerForKey: { defaults, key in defaults.integer(forKey: key) },
-            objectForKey: { defaults, key in defaults.object(forKey: key) },
-            register: { defaults, info in defaults.register(defaults: info) },
-            remove: { defaults, key in defaults.removeObject(forKey: key) },
-            setBool: { defaults, value, key in defaults.set(value, forKey: key) },
-            setData: { defaults, value, key in defaults.set(value, forKey: key) },
-            setDouble: { defaults, value, key in defaults.set(value, forKey: key) },
-            setInteger: { defaults, value, key in defaults.set(value, forKey: key) },
-            setObject: { defaults, value, key in defaults.set(value, forKey: key) },
-            setString: { defaults, value, key in defaults.set(value, forKey: key) },
-            stringForKey: { defaults, key in defaults.string(forKey: key) }
+            arrayForKey: { defaults, key in
+                let array = defaults.array(forKey: key)
+                Self.logger.debug("Getting array for key '\(key)': \(String(describing: array), privacy: .auto)")
+                return array
+            },
+            boolForKey: { defaults, key in
+                let bool = defaults.bool(forKey: key)
+                Self.logger.debug("Getting bool for key '\(key)': \(String(describing: bool), privacy: .auto)")
+                return bool
+            },
+            dataForKey: { defaults, key in
+                let data = defaults.data(forKey: key)
+                Self.logger.debug("Getting data for key '\(key)': \(String(describing: data), privacy: .auto)")
+                return data
+            },
+            doubleForKey: { defaults, key in
+                let double = defaults.double(forKey: key)
+                Self.logger.debug("Getting double for key '\(key)': \(String(describing: double), privacy: .auto)")
+                return double
+
+            },
+            integerForKey: { defaults, key in
+                let integer = defaults.integer(forKey: key)
+                Self.logger.debug("Getting integer for key '\(key)': \(String(describing: integer), privacy: .auto)")
+                return integer
+            },
+            objectForKey: { defaults, key in
+                let object = defaults.object(forKey: key)
+                Self.logger.debug("Getting object for key '\(key)': \(String(describing: object), privacy: .auto)")
+                return object
+            },
+            register: { defaults, info in
+                Self.logger.debug("Registering defaults: \(String(describing: info), privacy: .auto)")
+                defaults.register(defaults: info)
+            },
+            remove: { defaults, key in
+                Self.logger.debug("Removing item for key '\(key)'")
+                defaults.removeObject(forKey: key)
+            },
+            setBool: { defaults, value, key in
+                Self.logger.debug("Setting bool '\(String(describing: value), privacy: .auto))' for key '\(key)'")
+                defaults.set(value, forKey: key)
+            },
+            setData: { defaults, value, key in
+                Self.logger.debug("Setting data '\(String(describing: value), privacy: .auto))' for key '\(key)'")
+                defaults.set(value, forKey: key)
+            },
+            setDouble: { defaults, value, key in
+                Self.logger.debug("Setting double '\(String(describing: value), privacy: .auto))' for key '\(key)'")
+                defaults.set(value, forKey: key)
+            },
+            setInteger: { defaults, value, key in
+                Self.logger.debug("Setting integer '\(String(describing: value), privacy: .auto))' for key '\(key)'")
+                defaults.set(value, forKey: key)
+            },
+            setObject: { defaults, value, key in
+                Self.logger.debug("Setting object '\(String(describing: value), privacy: .auto))' for key '\(key)'")
+                defaults.set(value, forKey: key)
+            },
+            setString: { defaults, value, key in
+                Self.logger.debug("Setting string '\(String(describing: value), privacy: .auto))' for key '\(key)'")
+                defaults.set(value, forKey: key)
+            },
+            stringForKey: { defaults, key in
+                let string = defaults.string(forKey: key)
+                Self.logger.debug("Getting string for key '\(key)': \(String(describing: string), privacy: .auto)")
+                return string
+            }
         )
     }()
 
